2452f52d36603df68d01954f83bb783891084787
mastrhigginz <126441038+mastrhigginz@users.noreply.github.com>
2023-02-27 12:27:23 -0500

    Update bot.py
    Adding Fwd PE

--- bot.py
+++ bot.py
@@ -18,7 +18,7 @@ import random
 import numpy as np
 import configparser
 
-testing = False
+testing = True
 
 help_text = """
 
@@ -212,7 +212,7 @@ def Do_Equity_Reply(jsonData):
             marketCap = jsonData["price"]["marketCap"]["fmt"]
         except:
             marketCap = "N/A"
-        try:
+        '''try:
             trailingPERaw = jsonData["summaryDetail"]["trailingPE"]["raw"]
             trailingPEFmt = jsonData["summaryDetail"]["trailingPE"]["fmt"]
             if 0 <= trailingPERaw <= 15:
@@ -223,7 +223,20 @@ def Do_Equity_Reply(jsonData):
                 peColor = ':yellow_circle:'
             trailingPE = trailingPEFmt + peColor
         except:
-            trailingPE = "N/A"
+            trailingPE = "N/A"'''
+        #adding FwdPE
+        try:
+            FwdPERaw = jsonData["defaultKeyStatistics"]["forwardPE"]['raw']
+            FwdPEFmt = jsonData["defaultKeyStatistics"]["forwardPE"]['fmt']
+            if 0 <= FwdPERaw <= 15:
+                peColor = ':green_circle:'
+            elif FwdPERaw < 0 or FwdPERaw > 50:
+                peColor = ':red_circle:'
+            else:
+                peColor = ':yellow_circle:'
+                FwdPE = FwdPEFmt + peColor
+        except:
+            FwdPE = "N/A"
         try:
             pegRatioRaw = jsonData["defaultKeyStatistics"]["pegRatio"]["raw"]
             pegRatioFmt = jsonData["defaultKeyStatistics"]["pegRatio"]["fmt"]
@@ -386,6 +399,9 @@ def Do_Equity_Reply(jsonData):
             message.add_field(name="Last 52 Week Range", value=fiftyTwoWeekRange, inline=True)
         if trailingPE != "N/A":
             message.add_field(name="PE Ratio (ttm)", value=trailingPE, inline=True)
+        #add FwdPEfield
+        if FwdPE != "N/A":
+            message.add_field(name="PE Ratio (Fwd)", value=FwdPE, inline=True)
         if pegRatio != "N/A":
             message.add_field(name="PEG Ratio", value=pegRatio, inline=True)
         if priceToBook != "N/A":
